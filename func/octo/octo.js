d856c37ae47e51e7ffaa7d9dcd9c5f1c286e11ad
Marie-Elize Venter <ferplie@gmail.com>
2022-08-17 20:23:58 +0200

    Fourth

--- func/octo/octo.js
+++ func/octo/octo.js
@@ -3,13 +3,26 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context, callback) {
-  let url = 'https://api.github.com/users/octocat/repos?type=owner';
+  var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer 1000.d285d6260b770cda9dddb6b1ff22e330.27e555af37fff8e8aaec0d2e7b7bc989");
+myHeaders.append("Cookie", "442b5845d7=7252427f3b265480bf287607c0af6dd2; ZCNEWLIVEUI=true; _zcsr_tmp=5cc8dd11-8476-4cbc-84e3-9100047d1f70; zccpn=5cc8dd11-8476-4cbc-84e3-9100047d1f70");
 
-  let res = await fetch(url);
-  let data = await res.json();
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://creator.zoho.com/api/v2/zoho_henks/bainsvleicpf/report/All_Sectors/2573309000006717012", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+  // let res = await fetch(url);
+  // let data = await res.json();
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify(result),
   });
 };

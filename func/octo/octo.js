7f495198619f25b9333f9da2ba3d46a5327730d8
Marie-Elize Venter <ferplie@gmail.com>
2022-08-17 20:33:10 +0200

    sixth

--- func/octo/octo.js
+++ func/octo/octo.js
@@ -1,28 +1,40 @@
 // /.netlify/functions/octo
 
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-exports.handler = async function (event, context, callback) {
-  var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer 1000.d285d6260b770cda9dddb6b1ff22e330.27e555af37fff8e8aaec0d2e7b7bc989");
-myHeaders.append("Cookie", "442b5845d7=7252427f3b265480bf287607c0af6dd2; ZCNEWLIVEUI=true; _zcsr_tmp=5cc8dd11-8476-4cbc-84e3-9100047d1f70; zccpn=5cc8dd11-8476-4cbc-84e3-9100047d1f70");
+// exports.handler = async function (event, context, callback) {
+//   var myHeaders = new Headers();
+// myHeaders.append("Authorization", "Bearer 1000.d285d6260b770cda9dddb6b1ff22e330.27e555af37fff8e8aaec0d2e7b7bc989");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
 
-fetch("https://creator.zoho.com/api/v2/zoho_henks/bainsvleicpf/report/All_Sectors/2573309000006717012", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// fetch("https://creator.zoho.com/api/v2/zoho_henks/bainsvleicpf/report/All_Sectors/2573309000006717012", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
 
-  // let res = await fetch(url);
-  // let data = await res.json();
+//   // let res = await fetch(url);
+//   // let data = await res.json();
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  });
+//   callback(null, {
+//     statusCode: 200,
+//     body: JSON.stringify(result),
+//   });
+// };
+const fetch = require("node-fetch");
+
+const API_ENDPOINT = "https://creator.zoho.com/api/v2/zoho_henks/bainsvleicpf/report/All_Sectors/2573309000006717012";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json", Authorization: "Bearer 1000.d285d6260b770cda9dddb6b1ff22e330.27e555af37fff8e8aaec0d2e7b7bc989"} })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data.joke,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
